36f71f70b7d980f5d07655be297cd05fb7133f9c
Evans <akrap147@naver.com>
2025-11-18 11:26:08 +0900

    Initial Project SetUp

---
+++ src/main/java/dev/akrap/algobot/AlgoReviewApplication.java
@@ -0,0 +1,13 @@
+package dev.akrap.algobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/akrap/algobot/AlgoReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akrap.algobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
