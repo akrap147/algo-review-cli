29959bd6479bb16d7f5917996d4492cfed53e9f8
Evans <akrap147@naver.com>
2025-11-18 14:12:11 +0900

    feat: review model 완성 review 날짜 Switch case로직으로 만들어서 1,3,7,14,30 ... 등의 로직, stage ++로 완성한다.

---
+++ app/src/main/java/dev/akrap/algobot/model/ReviewItem.java
@@ -0,0 +1,90 @@
+package dev.akrap.algobot.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewItem {
+
+    private int problemId;                 // 문제 번호
+    private int stage;                     // 현재 복습 단계(1, 2, 3...)
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate nextReviewDate;      // 다음 복습 날짜
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;           // 처음 공부한 날짜
+
+
+    public ReviewItem() {
+
+    }
+
+    public ReviewItem(int problemId) {
+        this.problemId = problemId;
+        this.stage = 1;
+        this.createdAt = LocalDate.now();
+        this.nextReviewDate = LocalDate.now().plusDays(1); // stage=1 => +1일
+    }
+
+    public void completeReview() {
+        stage++;
+        int nextGap = getGapByStage(stage);
+        this.nextReviewDate = LocalDate.now().plusDays(nextGap);
+    }
+
+
+    // stage별로 며칠뒤에 하게 될지 정한다.
+    private int getGapByStage(int stage) {
+        return switch (stage) {
+            case 1 -> 1;
+            case 2 -> 3;
+            case 3 -> 7;
+            case 4 -> 14;
+            case 5 -> 30;
+            default -> 30;
+        };
+    }
+
+    public int getProblemId() {
+        return problemId;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public List<String> getHistory() {
+        return history;
+    }
+
+    public LocalDate getNextReviewDate() {
+        return nextReviewDate;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setProblemId(int problemId) {
+        this.problemId = problemId;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    public void setHistory(List<String> history) {
+        this.history = history;
+    }
+
+    public void setNextReviewDate(LocalDate nextReviewDate) {
+        this.nextReviewDate = nextReviewDate;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
